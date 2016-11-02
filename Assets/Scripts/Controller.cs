fd13e4b88acef5a1e3811ee48f80dbaa202036b8
Benjamin Hay <bhay@carpenter-04.elon.edu>
2016-11-01 23:31:18 -0400

    Mandatory Minimum

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour {
+	
+	[SerializeField] private TargetScript targetPrefab;
+	[SerializeField] private Text scoreText;
+	[SerializeField] private Text livesText;
+	[SerializeField] private Text matterText;
+
+	private float offsetX = 3.0f;
+	private float offsetZ = 5.0f;
+	private int score;
+	private int lives;
+	private float matter;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		lives = 3;
+
+		Vector3 startPos = targetPrefab.transform.position;
+		Vector3 pos = Vector3.zero;
+		for (int i = 0; i < 6; i++) {
+			TargetScript target;
+
+			if (i == 0) {
+				target = targetPrefab;
+			} else {
+				target = Instantiate (targetPrefab) as TargetScript;
+			}
+
+			float posZ = -(offsetX * i) + startPos.z;
+			target.transform.position = new Vector3 (startPos.x, startPos.y, posZ);
+		}
+
+		scoreText.text = "Score: " + score;
+		livesText.text = "Lives: " + lives;
+		matterText.text = "Matter: " + matter;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void setScore(int newScore){
+		score = newScore;
+	}
+
+	public void setLives(int newLives){
+		lives = newLives;
+	}
+
+	public void setMatter(float newMatter){
+		matter += newMatter;
+		UpdateUI();
+	}
+
+	public float getMatter(){
+		return matter;
+	}
+
+	public void UpdateUI(){
+		matterText.text = "Matter: " + matter;
+//		livesText.text = "Lives: " + lives;
+//		matterText.text = "Matter: " + matter;
+	}
+		
+}
---
+++ Assets/Scripts/RayShooter.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayShooter : MonoBehaviour {
+
+    private Camera camera;
+	[SerializeField] private Bullet bulletPrefab;
+	protected GameObject controllerObject;
+	protected Controller controller;
+	[SerializeField] public GameObject selectedObject;
+	private GameObject currentObj;
+
+    void Start() {
+        camera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+
+		controllerObject = GameObject.Find ("Controller");
+		controller = controllerObject.GetComponent<Controller>();
+
+    }
+
+    void Update() {
+		float scaleValue = Input.GetAxis ("Mouse ScrollWheel");
+
+		//eat stuff
+        if (Input.GetMouseButtonDown(1)) {
+            Vector3 origin = new Vector3(camera.pixelWidth / 2,camera.pixelHeight / 2,0);
+			Vector3 pos = transform.position;
+            Ray ray = camera.ScreenPointToRay(origin);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+				Collider col = hit.collider;
+				string tag = col.gameObject.tag;
+				if(tag == "SquareMatter"){
+					Vector3 size = col.bounds.size;
+					controller.setMatter (size.x*size.y*size.z);
+//					AddType ();
+//					selectedObject = col.gameObject;
+					Destroy (col.gameObject);
+				}
+            }
+        }
+		//shoot stuff
+		if (Input.GetMouseButtonDown (0)) {
+//			if (selectedObject != null && controller.getMatter() > 0){
+				Vector3 origin = new Vector3 (camera.pixelWidth / 2, camera.pixelHeight / 2, 0);
+				Vector3 pos = transform.position;
+				Ray ray = camera.ScreenPointToRay (origin);
+				RaycastHit hit;
+				if (Physics.Raycast (ray, out hit)) {
+					Collider col = hit.collider;
+					Shoot (hit.point);
+
+//				}
+			}
+		}
+
+		//scale stuff
+		if(scaleValue > 0){
+			currentObj.transform.localScale += new Vector3(0.1F, 0.1f, 0.1f);
+		} else if (scaleValue < 0){
+			currentObj.transform.localScale -= new Vector3(0.1F, 0.1f, 0.1f);
+		}
+    }
+
+    private void Shoot(Vector3 position) {
+		GameObject bullet = Instantiate (selectedObject);
+		Debug.Log ("shoot");
+        
+		Collider col = bullet.GetComponent<Collider> ();
+		Vector3 size = col.bounds.size;
+		float sizef = size.x * size.y * size.z;
+		if (controller.getMatter () > sizef) {
+			controller.setMatter (-sizef);
+			bullet.transform.position = position;
+			currentObj = bullet;
+		} else {
+			Destroy (bullet);
+		}
+//        bullet.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+//        yield return new WaitForSeconds(1);
+//		bullet.Remove();
+    }
+
+}
+
