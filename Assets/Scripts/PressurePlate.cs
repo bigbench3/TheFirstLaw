c8fe8bd387462cc8ead548ef8a061156219425ee
Benjamin Hay <bhay@carpenter-04.elon.edu>
2016-12-07 22:44:26 -0500

    tutorial shit

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision col){
+		GameObject obj = col.gameObject;
+		if (obj.tag == "Player") {
+			
+		}
+	}
+}
