90388cc97c07add6e5c6b7d4eaf22ef1dd8ce13f
Colin Hiriak <chiriak@carpenter-03.elon.edu>
2016-12-08 03:00:49 -0500

    door working but closes too fast, help with coroutine

---
+++ Assets/Scripts/OpenDoors.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoors : MonoBehaviour {
+
+	// To open and close doors via Inspector
+	private bool Open_Doors;
+	private bool Close_Doors;
+
+	// Once the doors has been opened they can be closed again
+	private bool door_opened;
+
+
+	public GameObject BottomDoors;
+	public GameObject TopDoors;
+
+	// Store original position of top and bottom doors
+	Vector3 TopDoorsOriginPosition;
+	Vector3 BottomDoorsOriginPosition;
+
+	void Awake()
+	{
+		TopDoorsOriginPosition = TopDoors.transform.position;
+		BottomDoorsOriginPosition = BottomDoors.transform.position;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Open_Doors = false;
+		Close_Doors = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Open_Doors)
+		{
+			OpenSciFiDoors();
+			StartCoroutine (Hold ());
+		}
+
+		if(door_opened && Close_Doors)
+		{
+			StartCoroutine (Hold ());
+			CloseSciFiDoors();
+		}
+
+	}
+
+
+	public void OpenSciFiDoors()
+	{
+		TopDoors.transform.position = Vector3.Lerp (TopDoors.transform.position, 
+			new Vector3(TopDoors.transform.position.x, TopDoors.transform.position.y, TopDoors.transform.position.z) + 
+			new Vector3 (0, 6f,TopDoors.transform.position.z ), 0.45f * Time.deltaTime);
+		BottomDoors.transform.position = Vector3.Lerp (BottomDoors.transform.position, 
+			new Vector3(BottomDoors.transform.position.x, BottomDoors.transform.position.y, BottomDoors.transform.position.z) + 
+			new Vector3 (0, -3.310805f, BottomDoors.transform.position.z), 0.45f * Time.deltaTime);
+
+		// We want to wait till doors reach certain position before they can be closed
+		if(TopDoors.transform.position.y > 3 && BottomDoors.transform.position.y < -2.5f)
+		{
+			door_opened = true;
+			Open_Doors = false;
+			StartCoroutine (Hold ());		
+		}
+	
+	}
+
+	void CloseSciFiDoors()
+	{
+		StartCoroutine (Hold ());		
+		TopDoors.transform.position = Vector3.Lerp (TopDoors.transform.position, TopDoorsOriginPosition, 0.45f * Time.deltaTime);
+		BottomDoors.transform.position = Vector3.Lerp (BottomDoors.transform.position, BottomDoorsOriginPosition, 0.45f * Time.deltaTime);
+
+	}
+
+	IEnumerator Hold() {
+		yield return new WaitForSecondsRealtime (3f);
+	}
+
+}
+
--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -2,21 +2,35 @@
 using System.Collections;
 
 public class PressurePlate : MonoBehaviour {
+	private OpenDoors opendoors;
+	[SerializeField] protected GameObject door;
 
 	// Use this for initialization
 	void Start () {
+		door = GameObject.Find("SciFiDoorsSep");
+		opendoors = door.GetComponent<OpenDoors> ();
+		
+		
+		
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
-	void OnCollisionEnter(Collision col){
-		GameObject obj = col.gameObject;
-		if (obj.tag == "Player") {
+	void OnTriggerStay(Collider col){
+		//GameObject obj = col.gameObject;
+		if (col.gameObject.tag == "Player") {
+			Debug.Log ("Pressure Plate hit");
+			opendoors.OpenSciFiDoors ();
+
+
+			
 			
 		}
+		//opendoors.OpenSciFiDoors();
+
 	}
 }
